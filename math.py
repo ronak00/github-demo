e70bccc418dd7d9efb9f22249b88049edc924948
Ronak Kothari <jainronak748@gmail.com>
2021-07-02 14:51:31 +0530

    implemented subtract() fn in main branch

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 # Subtract implementation
 def subtract(x,y):
-    pass                
+    return x-y               
 # multipy implementation
 def multiply(x,y):
     pass                 
